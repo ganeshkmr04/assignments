0eeeb4c0b62586d66d875d03b0e57506ec8630f6
ganeshkmr04 <119889401+ganeshkmr04@users.noreply.github.com>
2022-12-16 18:17:44 +0530

    Update TwoEditLead.java

--- CodeForReview/Assignment_Week_2/TwoEditLead.java
+++ CodeForReview/Assignment_Week_2/TwoEditLead.java
@@ -31,10 +31,12 @@ public class TwoEditLead {
 		chrome.findElement(By.linkText("CRM/SFA")).click();
 		chrome.findElement(By.linkText("Leads")).click();
 		chrome.findElement(By.linkText("Find Leads")).click();
-
+		
+		//Enter a first name to search by name and submit.
 		chrome.findElement(By.xpath("//div[@class='x-tab-panel-bwrap']//input[@name='firstName']")).sendKeys("Ganesh");
 		chrome.findElement(By.xpath("//button[text()='Find Leads']")).click();
 		
+		//Click on first matched record from the search result
 		chrome.findElement(By.xpath("//div[@class='x-grid3-scroller']/div[1]/div[1]//tr/td[1]/div[1]/a")).click();
 
 		//Verifying the tile is View Lead
@@ -42,13 +44,14 @@ public class TwoEditLead {
 			//Click on Edit Lead link
 			chrome.findElement(By.linkText("Edit")).click();
 		}
-		String updatedCompany="Microsoft Inc";
 		
+		//Update the existing company name with new name and Click Update
+		String updatedCompany="Microsoft Inc";
 		chrome.findElement(By.id("updateLeadForm_companyName")).clear();
 		chrome.findElement(By.id("updateLeadForm_companyName")).sendKeys(updatedCompany);
 		chrome.findElement(By.xpath("//input[@value='Update']")).click();
-			
 		
+		//Verifying the company name is updated in view lead to confirm the updation.
 		if(chrome.findElement(By.id("viewLead_companyName_sp")).getText().contains(updatedCompany))
 			System.out.println("Update Successful");
 		
